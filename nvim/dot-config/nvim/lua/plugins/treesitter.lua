33aa9aea7bc7c6fcb574022510795960e0087d51
Terry Lockett <5551818+terrylockett@users.noreply.github.com>
2025-10-30 12:06:22 -0300

    neovim - treesitter fix

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,23 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = 'master',
 	lazy = false,
-	build = ":TSUpdate"
+	build = ":TSUpdate",
+	config = function()
+		local tsConfigs = require("nvim-treesitter.configs")
+		tsConfigs.setup({
+			ensure_installed = {
+				"go",
+				"lua",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true
+			},
+			indent = {
+				enable = true
+			},
+		})
+	end
 }

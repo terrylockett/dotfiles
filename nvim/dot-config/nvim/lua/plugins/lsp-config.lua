ecfed21d422cb45c353f911cb1960dc24f52c761
Terry Lockett <5551818+terrylockett@users.noreply.github.com>
2025-11-10 15:16:45 -0400

    neovim - update lsp-config to 0.11

--- nvim/dot-config/nvim/lua/plugins/lsp-config.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp-config.lua
@@ -2,41 +2,14 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			automatic_enable = {
-					"lua_ls",
-					"gopls",
-					"gradle_ls",
-					"groovyls",
-					-- "tsserver",
-					"rust_analyzer",
-					-- "kotlin_language_server",
-					"lemminx",
-					"pyright",
-					"taplo", -- toml
-					"jsonls",
-					"bashls"
+			ensure_installed = {
+				"gopls",
+				"lua_ls",
 			}
 		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
 		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require('lspconfig')
-			lspconfig.lua_ls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.gradle_ls.setup({})
-			-- lspconfig.tsserver.setup({})
-			lspconfig.rust_analyzer.setup({})
-			-- lspconfig.kotlin_language_server.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.taplo.setup({})
-			lspconfig.jsonls.setup({})
-			lspconfig.bashls.setup({})
-			lspconfig.yamlls.setup({})
-		end
 	}
 }

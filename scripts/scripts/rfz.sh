8d1e237736514426ffa0f7b9159ca7d135a4798b
Terry Lockett <5551818+terrylockett@users.noreply.github.com>
2026-01-06 14:39:12 -0400

    fix for rfz opening files in github

--- scripts/scripts/rfz.sh
+++ scripts/scripts/rfz.sh
@@ -6,6 +6,10 @@
 # 1. Search for text in files using Ripgrep
 # 2. Interactively narrow down the list using fzf
 # 3. Open the file in neovim or github
+#
+# This requires terry-uils/giturl to be installed
+# Link: https://github.com/terrylockett/terry-utils?tab=readme-ov-file#giturl
+#
 rg --color=always --line-number --no-heading --smart-case "${*:-}" |
 	fzf --ansi \
 	--color "hl:-1:underline,hl+:-1:underline:reverse" \
@@ -13,5 +17,5 @@ rg --color=always --line-number --no-heading --smart-case "${*:-}" |
 	--preview 'bat --color=always {1} --highlight-line {2}' \
 	--preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
 	--bind 'enter:become(nvim {1} +{2})' \
-	--bind 'ctrl-r:become(giturl {1} +{2})'
+	--bind 'ctrl-r:become(giturl -o {1})'
 
